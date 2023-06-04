0f72401bfbb7387804374348b1a14e604956675c
Eddie <wilson.eddw@gmail.com>
2023-06-04 21:07:48 +0700

    Added bar padding patch

--- config.h
+++ config.h
@@ -6,6 +6,8 @@ static const unsigned int gappx     = 18;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "terminus:size=16", "fontawesome:size=16" };
 static const char dmenufont[]       = "terminus:size=16";
 static const char col_gray1[]       = "#010030";
